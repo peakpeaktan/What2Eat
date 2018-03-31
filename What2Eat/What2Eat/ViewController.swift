3960b939f951e2c14d577a05bc67cf6c771b7b7a
Zhifeng Tan <tan_zhif@bentley.edu>
2018-03-31 12:14:49 -0400

    Initial commit

---
+++ What2Eat/What2Eat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What2Eat
+//
+//  Created by zhifeng on 3/31/18.
+//  Copyright © 2018 Zhifeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
